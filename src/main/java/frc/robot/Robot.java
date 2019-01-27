562dd29f8ecd0ded6117f738538bf1b84a57a419
6164DevComputer1 <6164DevComputer1@gmail.com>
2019-01-27 10:14:52 -0600

    Added comments, Power Distribution panel and a voltage monitor

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
@@ -30,22 +31,25 @@ public class Robot extends TimedRobot {
   AnalogGyro gyro;
   Compressor comp;
  MecanumDrive m_myRobot;
+ PowerDistributionPanel pdp;
+ 
 
   @Override
   public void robotInit() {
-   BL = new WPI_VictorSPX(1);
+   BL = new WPI_VictorSPX(1);         // Motors and where they are plugged into the bot
    BR = new WPI_VictorSPX(2);
    FR = new WPI_VictorSPX(3);
    FL = new WPI_VictorSPX(4);
-   drive = new MecanumDrive(FR, BL, FR, BR);
+   drive = new MecanumDrive(FR, BL, FR, BR); // stating the drive type for the bot
    drive.setSafetyEnabled(false);
-   controller = new Joystick(0);
-   sol1 = new Solenoid(0);
+   controller = new Joystick(0);           // creating the controller
+   sol1 = new Solenoid(0);                 // Creating both ends of the Solenoid base
    sol2 = new Solenoid(1);
-   gyro = new AnalogGyro(0);
-   comp = new Compressor(0);
-  m_myRobot = new MecanumDrive(FL, BL, FR, BR);
-
+   gyro = new AnalogGyro(0);               // creating Gyro
+   comp = new Compressor(0);              // creating Compressor
+  pdp = new PowerDistributionPanel();
+  
+  
 
   }
 
@@ -70,7 +74,8 @@ public class Robot extends TimedRobot {
  
   @Override
   public void teleopPeriodic() {
-    m_myRobot.driveCartesian(controller.getX(), controller.getY(), controller.getZ());
+    System.out.println("Battery voltage is at" + pdp.getVoltage());
+    drive.driveCartesian(controller.getX(), controller.getY(), controller.getZ());
     if(controller.getRawButton(1)){
       sol1.set(true);
       }
