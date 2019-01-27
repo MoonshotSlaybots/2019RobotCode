005da728af447a1282edc01988bcc680c45d1089
C-Glick <colton.glick@gmail.com>
2019-01-27 14:58:48 -0600

    edited the piston comments

--- src/main/java/frc/robot/Piston.java
+++ src/main/java/frc/robot/Piston.java
@@ -4,14 +4,14 @@ import edu.wpi.first.wpilibj.Solenoid;
 
 
 public class Piston{
-    Solenoid sideA;             //portA and portB are the ports that the solenoid is pluged into for this piston
-    Solenoid sideB;             //this class was made with a double solenoid in mind,
-                                //activate one side to extend the pistion, then activate the other side to retract
-
+    Solenoid sideA;             
+    Solenoid sideB;             
+                                
 
     Piston(int portA,int portB){             //constructor for the Piston Class
-        sideA = new Solenoid(portA);         //portA and portB are the ports that the solenoid is pluged into for this piston
-        sideB = new Solenoid(portB);         //this class was made with a double solenoid in mind,
+        sideA = new Solenoid(portA);         //portA and portB are the ports that the valve is pluged into for this piston
+        sideB = new Solenoid(portB);         //this class was made with a double solenoid valve in mind,
+                                             //each valve has two solenoids to control
                                              //activate one side to extend the pistion, then activate the other side to retract
     }
 
